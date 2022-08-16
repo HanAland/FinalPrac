0a50cf7dd64f6a704c4038e58a7b5bf52978df20
Hyeyoung Park <parhyeyo@sheridancollege.ca>
2022-08-16 10:01:08 -0400

    Add part time student

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,25 @@
+
+package student;
+
+/**
+ *
+ * @author hannah
+ */
+public class PartTimeStudent extends Student {
+    private int numCourses;
+
+    public PartTimeStudent(String name, int numCourses) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
